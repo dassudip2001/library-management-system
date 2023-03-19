311afcbe02aa00ebb9d34366096876cf4547c405
Sudip Das <sudip5428@gmail.com>
2023-03-19 20:46:34 +0530

    code commit for admin account setup

--- lms/database/seeders/UserSeeder.php
+++ lms/database/seeders/UserSeeder.php
@@ -2,22 +2,21 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
         $user = new User();
         $user->name =            "Super Admin";
-        $user->email =            "admin@lms.com";
+        $user->email =            "admin@rsbs.com";
         $user->password =           Hash::make('password');
         $user->save();
     }
